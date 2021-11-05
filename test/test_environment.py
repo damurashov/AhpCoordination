7467189f227f2c8cdaa3dc962104d453dcb39e5b
DmitryMurashov <dmtr.murashov@gmail.com>
2021-11-05 13:01:33 +0300

    [test_env] Impl

---
+++ test/test_environment.py
@@ -0,0 +1,42 @@
+import unittest
+from pathlib import Path
+import sys
+
+sys.path.insert(0, str(Path(__file__).parent.parent / "ahpcoord"))
+
+from environment import *
+from generic import Log
+
+
+class TestWorld(unittest.TestCase):
+
+	def setUp(self):
+		print()
+
+		self.n_agents = 10
+		self.factory = WorldFactory(
+			world_dim=[10, 10],
+			n_teams=2,
+			hitter_energy_mean=5,
+			hitter_energy_deviation=1,
+			resource_energy_mean=5,
+			resource_energy_deviation=1,
+		)
+		self.world = World()
+
+		for _ in range(self.n_agents):
+			agent = self.factory.gen_hitter()
+			Log.debug(self.setUp, "generated agent", str(agent))
+			self.world.add_agent(agent)
+
+		Log.debug(self.setUp,  "world.n_agents", self.world.calc_agents())
+
+	def test_load_save(self):
+		self.world.save("echo")
+		del self.world
+		self.world = World()
+		self.world.load("echo")
+
+		self.assertTrue(self.world.calc_agents() == self.n_agents)
+
+
